1aeca3097ffec7d371fba2ae50f9075537e065c4
Maciej Ogonowski <maciejogonowski1@gmail.com>
2021-09-07 00:17:02 +0200

    Commit test

---
+++ ProgramowanieZabawa1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProgramowanieZabawa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czesc zaczynamy zabawe");
+            int wykonywac = 1;
+
+            if (wykonywac == 0)
+            {
+                double a, b, c;
+                string liczba;
+                Console.WriteLine("Podaj liczbe a");
+                liczba = Console.ReadLine();
+                a = Double.Parse(liczba);
+                Console.WriteLine("Podaj liczbe b");
+                liczba = Console.ReadLine();
+                b = Double.Parse(liczba);
+                c = a + b;
+                Console.WriteLine(c);
+
+                if (c > 2)
+                {
+                    Console.WriteLine("Nie wiem");
+                }
+                else
+                {
+                    Console.WriteLine("wiem");
+                }
+            }
+
+//zmiana
+
+
+        }
+    }
+}

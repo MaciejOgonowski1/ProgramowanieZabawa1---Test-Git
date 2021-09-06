53444c8fd613b10e9446d991238e8c24525d610d
Maciej Ogonowski <maciejogonowski1@gmail.com>
2021-09-07 00:45:49 +0200

    Edit 2

--- ProgramowanieZabawa1/Program.cs
+++ ProgramowanieZabawa1/Program.cs
@@ -31,6 +31,38 @@ namespace ProgramowanieZabawa1
                     Console.WriteLine("wiem");
                 }
             }
+            /* Komentarz
+             * 
+             */
+            // sbyte -128 -> 127
+            sbyte znak = 127;
+            // short -32768 -> 32767
+            // int -2mld -> 2mld 4 bajty
+            // long -2^63 do 2^63-1 8 bajtów
+            // byte - 0 do 255 na 8 bitach
+            // ushort (unsigned) - 0 : 65535
+
+            /*
+             * Zmienno przecinkowe
+             * float 7 znaków po przecinku
+             * 
+             * double od 15-16
+             * decimal 28 znaków po przecinku
+             * 
+             * 
+             * boolean True/False
+             * 
+             */
+            float zmienna = 2.1234567890F;
+            Console.WriteLine(zmienna);
+
+            Boolean PrawdaFalsz;
+            PrawdaFalsz = true;
+            Console.WriteLine(PrawdaFalsz);
+
+            // String
+            String imie = "Maciek";
+            Console.WriteLine(imie);
 
         }
     }
